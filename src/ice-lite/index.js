fdfef6715719a2b795afef4292c2cda24d9210fa
dingyiming <315129552@qq.com>
2018-03-17 17:46:28 +0800

    添加ice 其他模板

---
+++ src/ice-lite/index.js
@@ -0,0 +1,21 @@
+
+import Vue from 'vue'
+import { VuePlugin } from 'vuera'
+import ReactDOM from 'react-dom';
+// 载入默认全局样式 normalize 、.clearfix 和一些 mixin 方法等
+import '@icedesign/base/reset.scss';
+import '@icedesign/base/dist/ICEDesignBase.css';
+import '@icedesign/layout/lib/main.scss';
+import '@icedesign/menu/lib/main.scss';
+import App from '../ice/scaffolds/ice-design-lite/src/routes'
+
+Vue.use(VuePlugin)
+Vue.config.productionTip = false
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@
 const entry = [
   {
     path: 'ice-cms',
+    name: 'cms'
+  },
+  {
+    path: 'ice-lite',
+    name: 'lite'
+  },
+  {
+    path: 'ice-pro',
+    name: 'pro'
+  },
+  {
+    path: 'ice-analytical',
     name: 'index'
   }
 ]
